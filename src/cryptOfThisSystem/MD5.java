8ba6a13f3e7b0cb08c2530e7709c450e2646858f
G-WS <48270961+G-WS@users.noreply.github.com>
2022-07-21 17:41:29 +0800

    Update MD5.java

--- src/cryptOfThisSystem/MD5.java
+++ src/cryptOfThisSystem/MD5.java
@@ -30,6 +30,7 @@ public class MD5 {
                     0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
                     0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
                     0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391}};
+    
     static final int k[][] = {
             {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
             {1, 6, 11, 0, 5, 10, 15, 4, 9, 14, 3, 8, 13, 2, 7, 12},
@@ -143,12 +144,12 @@ public class MD5 {
             result[n] &= 0xFFFFFFFFL;
         }
     }
-
+//MD5 test
     public static void main(String[] args) {
         MD5 md = new MD5();
         String message = "this is zhou";
-        System.out.println("MD5-Algorithm:\n\n消息: " + message);
-        System.out.println("MD5消息: " + md.start(message));
+        System.out.println("MD5-Algorithm:\n\n脧没脧垄: " + message);
+        System.out.println("MD5脧没脧垄: " + md.start(message));
 
     }
 }
